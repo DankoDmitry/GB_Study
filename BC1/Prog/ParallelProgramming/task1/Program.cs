fcc9e73e7357445143f50c657df7a4f28d9a5c93
Dmirty <ddigorev@gmail.com>
2022-08-11 15:16:31 +0300

    added BC1

---
+++ BC1/Prog/ParallelProgramming/task1/Program.cs
@@ -0,0 +1,78 @@
+﻿const int N = 100;
+const int THREADS_NUMBER = 8;
+
+int[,] serialMulRes = new int[N, N];
+int[,] threadMulRes = new int[N, N];
+
+int[,] firstMatrix = MatrixGenerator(N, N);
+int[,] secondMatrix = MatrixGenerator(N, N);
+
+SerialMatrixMul(firstMatrix, secondMatrix);
+ParallelMatrixMul(firstMatrix, secondMatrix);
+
+
+int[,] MatrixGenerator(int rows, int columns)
+{
+    Random _rand = new Random();
+    int[,] res = new int[rows, columns];
+    for (int i = 0; i < res.GetLength(0); i++)
+    {
+        for (int j = 0; j < res.GetLength(1); j++)
+        {
+            res[i, j] = _rand.Next(-100, 100);
+        }
+    }
+    return res;
+}
+
+void SerialMatrixMul(int[,] a, int[,] b)
+{
+    if (a.GetLength(1) != b.GetLength(0)) throw new System.Exception("Matrix multiplication failed");
+
+    for (int i = 0; i < a.GetLength(0); i++)
+    {
+        for (int j = 0; j < b.GetLength(1); j++)
+        {
+            for (int k = 0; k < a.GetLength(0); k++)
+            {
+                serialMulRes[i, j] += a[i, k] * b[k, j];
+            }
+        }
+    }
+}
+
+void PrepareParallelMatrixMul(int[,] a, int[,] b)
+{
+    if (a.GetLength(1) != b.GetLength(0)) throw new System.Exception("Matrix multiplication failed");
+
+    int eachThreadCalc = N / THREADS_NUMBER;
+    var threadslist = new List<Thread>();
+    for (int i = 0; i < THREADS_NUMBER; i++)
+    {
+        int startpos = i * eachThreadCalc;
+        int endPos = (i + 1) * eachThreadCalc;
+
+        if (i == THREADS_NUMBER - 1) endPos = N;
+        threadslist.Add(new Thread(() => ParallelMatrixMul(a, b, startpos, endPos)));
+        threadslist[i].Start();
+    }
+    for (int i = 0; i < THREADS_NUMBER; i++)
+    {
+        threadslist[i].Join();
+    }
+}
+
+
+void ParallelMatrixMul(int[,] a, int[,] b, int startPos, int endPos)
+{
+    for (int i = startPos; i < endPos; i++)
+    {
+        for (int j = 0; j < b.GetLength(1); j++)
+        {
+            for (int k = 0; k < a.GetLength(0); k++)
+            {
+                threadMulRes[i, j] += a[i, k] * b[k, j];
+            }
+        }
+    }
+}
---
+++ BC1/QuickSort/Task1/Program.cs
@@ -0,0 +1,42 @@
+﻿/* QuickSort
+1. Get the array: arr = {1, 0, -6, 2, 5, 3, 2}
+2. arr[6] ===== pivot (опорный элемент)
+3. Вызвать шаги 1-2 для подмассива слева от pivot и справа от pivot
+*/
+
+int[] arr = { 0, -5, 2, 3,5, 9, -1, 7};
+int[] res = QuiakSort(arr, 0, arr.Length - 1);
+
+Console.WriteLine($"Отсортированный массив {string.Join(", ", res)}");
+
+int[] QuiakSort(int[] inputArray, int minIndex, int maxIndex)
+{
+    if (minIndex >= maxIndex) return inputArray;
+    int pivot = GetPivotindex(inputArray, minIndex, maxIndex);
+    QuiakSort(inputArray, minIndex, pivot - 1);
+    QuiakSort(inputArray, pivot + 1, maxIndex);
+    return inputArray;
+}
+
+int GetPivotindex(int[] inputArray, int minIndex, int maxIndex)
+{
+    int pivotIndex = minIndex - 1;
+    for (int i = minIndex; i <= maxIndex - 1; i++)
+    {
+        if (inputArray[i] < inputArray[maxIndex])
+        {
+            pivotIndex++;
+            Swap(inputArray, i, pivotIndex);
+        }
+    }
+    pivotIndex++;
+    Swap(inputArray, pivotIndex, maxIndex);
+    return pivotIndex;
+}
+
+void Swap(int[] inpurarray, int leftValue, int rightValue)
+{
+    int temp = inpurarray[leftValue];
+    inpurarray[leftValue] = inpurarray[rightValue];
+    inpurarray[rightValue] = temp;
+}
